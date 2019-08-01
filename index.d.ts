2c82bc017485886715e90b6b8dc3b53b9c8977c5
Ayon Lee <ayonlys@gmail.com>
2019-08-01 17:24:52 +0800

    initial commit

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 type ErrorObject = Required<Error> & { [prop: string]: any };
 
-declare export function err2obj(err: any): ErrorObject;
-declare export function obj2err(err: any): ErrorObject;
+export declare function err2obj(err: any): ErrorObject;
+export declare function obj2err(err: any): ErrorObject;
